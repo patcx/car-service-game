f4532b76b4e6982f834e8be7ffcca758de33afa2
Karol <Karolbienko1210@gmail.com>
2018-01-04 00:26:55 +0100

    zapomniałem dodać do commita po zmergowaniu XD

--- CarServiceGameWeb/src/app/interfaces/order-service.ts
+++ CarServiceGameWeb/src/app/interfaces/order-service.ts
@@ -2,7 +2,6 @@ import { Observable } from "rxjs/Observable";
 import { Order } from "../model/order";
 
 export interface IOrderService {
-
     getHistoryOrders();
     updateOrders();
     getOrders(): Array<Order>;
